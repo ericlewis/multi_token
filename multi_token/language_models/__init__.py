b44e2e1bf672d82bc81f2f7fd862f086bdd944f7
Eric Lewis <ericlewis777@gmail.com>
2024-07-12 18:54:02 -0400

    phi3

--- multi_token/language_models/__init__.py
+++ multi_token/language_models/__init__.py
@@ -1,5 +1,6 @@
 from multi_token.language_models.mistral import MistralLMMForCausalLM
 from multi_token.language_models.qwen2 import Qwen2LMMForCausalLM
+from multi_token.language_models.phi3 import Phi3LMMForCausalLM
 
-LANGUAGE_MODEL_CLASSES = [MistralLMMForCausalLM, Qwen2LMMForCausalLM]
+LANGUAGE_MODEL_CLASSES = [MistralLMMForCausalLM, Qwen2LMMForCausalLM, Phi3LMMForCausalLM]
 LANGUAGE_MODEL_NAME_TO_CLASS = {cls.__name__: cls for cls in LANGUAGE_MODEL_CLASSES}
